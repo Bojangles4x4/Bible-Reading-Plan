18f5c67625645cd9b4e761c1bc9f0d17da92ddfc
Bojangles <tbbenefield@crimson.ua.edu>
2026-07-27 16:38:48 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="ten-category-bible-plan-v5-direct-chapter-audio";
+const CACHE_NAME="ten-category-bible-plan-v6-in-app-lsb-reader";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
